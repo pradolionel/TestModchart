997443b656be6b2ff7a5810de5da50a5f2eb7e6c
idktrolled <camilocarlos1100@gmail.com>
2024-10-12 20:47:58 -0500

    :trollface:

--- source/StorageUtil.hx
+++ source/StorageUtil.hx
@@ -1,29 +1,5 @@
 package;
 
-import animateatlas.AtlasFrameMaker;
-import flixel.math.FlxPoint;
-import flixel.graphics.frames.FlxFrame.FlxFrameAngle;
-import openfl.geom.Rectangle;
-import flixel.math.FlxRect;
-import haxe.xml.Access;
-import openfl.system.System;
-import flixel.FlxG;
-import flixel.graphics.frames.FlxAtlasFrames;
-import openfl.utils.AssetType;
-import openfl.utils.Assets as OpenFlAssets;
-import lime.utils.Assets;
-import flixel.FlxSprite;
-#if syss
-import sys.io.File;
-import sys.FileSystem;
-#end
-import flixel.graphics.FlxGraphic;
-import openfl.display.BitmapData;
-import haxe.Json;
-import flash.media.Sound;
-
-
-
 #if sys
 import sys.*;
 import sys.io.*;
@@ -31,6 +7,8 @@ import sys.io.*;
 import lime.system.System as LimeSystem;
 import haxe.io.Path;
 import haxe.Exception;
+
+using StringTools;
 
 @:structInit class SaveVariables {
 	#if android
